9942c1992bb06fa6cbe01513f99f948624e8b68d
Patryk <Zawadxki>
2020-01-18 21:06:19 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const endpoint = 'http://localhost:8081';
+const socket = socketIOClient(endpoint);
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+			room: ''
+    };
+	}
+	
+
+  componentDidMount() {
+    // const { endpoint } = this.state;
+		// const socket = socketIOClient(endpoint);
+		socket.on('roomName', (data) => {
+			this.setState({ room: data });
+		});
+	}
+	
+	onSearchGame = () => {
+		socket.emit('searchGame');
+		console.log('emitting');
+	}
+	
+  render() {
+		const { room } = this.state;
+    return (
+        <div style={{ textAlign: "center" }}>
+          <button onClick={this.onSearchGame}>SEARCH GAME </button>
+					{room};
+        </div>
+    );
+  }
 }
 
 export default App;
